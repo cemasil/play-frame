0eca1424d8ed32678e38c6cb237472042987ef4c
cembdc <cembdci@gmail.com>
2025-11-07 22:10:09 +0300

    Add GameSelectionPanel and MainMenuPanel for improved UI navigation and game selection

---
+++ Assets/Scripts/UI/Panels/GameSelectionPanel.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.UI;
+using MiniGameFramework.Systems.SceneManagement;
+using MiniGameFramework.Systems.SaveSystem;
+using MiniGameFramework.Systems.UI;
+using MiniGameFramework.UI.Prefabs;
+using MiniGameFramework.MiniGames;
+
+namespace MiniGameFramework.UI.Panels
+{
+    public class GameSelectionPanel : UIPanel
+    {
+        [Header("Container")]
+        [SerializeField] private Transform gameContainer;
+        [SerializeField] private Button backButton;
+
+        protected override void OnInitialize()
+        {
+            if (backButton != null)
+                backButton.onClick.AddListener(OnBackClicked);
+
+            PopulateGameCards();
+        }
+
+        private void PopulateGameCards()
+        {
+            if (gameContainer == null)
+            {
+                Debug.LogError("[GameSelectionPanel] Game Container is not assigned!");
+                return;
+            }
+
+            var games = GameRegistry.Instance.AvailableGames;
+
+            foreach (var game in games)
+            {
+                int highScore = SaveManager.Instance.GetGameHighScore(game.gameName);
+                var card = UIPrefabFactory.CreateGameCard(
+                    game.displayName,
+                    highScore,
+                    gameContainer
+                );
+
+                if (card == null)
+                {
+                    Debug.LogWarning($"[GameSelectionPanel] Failed to create card for {game.displayName}");
+                    continue;
+                }
+
+                var playButton = card.GetComponentInChildren<Button>();
+                if (playButton != null)
+                {
+                    playButton.onClick.AddListener(() =>
+                        SceneLoader.Instance.LoadScene(game.sceneName)
+                    );
+                }
+            }
+        }
+
+        private void OnBackClicked()
+        {
+            SceneLoader.Instance.LoadScene(SceneNames.MAIN_MENU);
+        }
+
+        protected override void OnCleanup()
+        {
+            if (backButton != null)
+                backButton.onClick.RemoveAllListeners();
+
+            if (gameContainer != null)
+            {
+                foreach (Transform child in gameContainer)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Panels/MainMenuPanel.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using MiniGameFramework.Systems.SceneManagement;
+using MiniGameFramework.Systems.UI;
+
+namespace MiniGameFramework.UI.Panels
+{
+    public class MainMenuPanel : UIPanel
+    {
+        [Header("Buttons")]
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button settingsButton;
+        [SerializeField] private Button quitButton;
+
+        protected override void OnInitialize()
+        {
+            if (playButton != null)
+                playButton.onClick.AddListener(OnPlayClicked);
+
+            if (settingsButton != null)
+                settingsButton.onClick.AddListener(OnSettingsClicked);
+
+            if (quitButton != null)
+                quitButton.onClick.AddListener(OnQuitClicked);
+        }
+
+        private void OnPlayClicked()
+        {
+            SceneLoader.Instance.LoadScene(SceneNames.GAME_SELECTION);
+        }
+
+        private void OnSettingsClicked()
+        {
+            Debug.Log("Settings clicked - TODO: Implement settings");
+        }
+
+        private void OnQuitClicked()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        protected override void OnCleanup()
+        {
+            if (playButton != null)
+                playButton.onClick.RemoveAllListeners();
+
+            if (settingsButton != null)
+                settingsButton.onClick.RemoveAllListeners();
+
+            if (quitButton != null)
+                quitButton.onClick.RemoveAllListeners();
+        }
+    }
+}
